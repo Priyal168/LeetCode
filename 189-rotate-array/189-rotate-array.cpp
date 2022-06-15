321b79fff92dc05d45363c4eb70effc03bb113c9
Priyal Khapekar <priyalkhapekar1442@gmail.com>
2022-06-15 10:54:23 +0530

    Time: 38 ms (67.48%), Space: 25 MB (53.86%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    void swapTheArray(vector<int>& nums, int f,int l){
-        while(f<l){
-            swap(nums[f],nums[l]);
-            f++;
-            l--;
-        }    
-    }
-    
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k=k%n;
-        swapTheArray(nums,0,n-1-k);
-        swapTheArray(nums,n-k,n-1);
-        swapTheArray(nums,0,n-1);
+        k %=nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
